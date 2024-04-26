18cd78bc6cd637b0f701bd83363ba6ec656ad9b2
bahmannikouei <nikoubahman@gmail.com>
2024-04-26 13:29:11 +0200

    Jalase 04

--- jalase04/jalase04-02.php
+++ jalase04/jalase04-02.php
@@ -3,17 +3,17 @@ $number1 = 50; // integer
 $number2 = 50; // integer
 $text = "50"; // string
 echo "<br>";
-var_dump("number1 == number2:" . $number1 == $number2); // true
-var_dump("number1 <= number2:" . $number1 <= $number2); // true
-var_dump("number1 >= number2:" . $number1 >= $number2); // true
-var_dump("number1 < number2:" . $number1 < $number2); // false
-var_dump("number1 > number2:" . $number1 > $number2); // false
-var_dump("number1 === number2:" . $number1 === $number2); // true
-var_dump("text == number2:" . $text == $number2); // true
-var_dump("text === number2:" . $text === $number2); // false
-var_dump("number1 != number2:" . $number1 != $number2); // false
-var_dump("number1 !== number2:" . $number1 !== $number2); // false
-var_dump("text !== number2:" . $text !== $number2); // true
+var_dump("number1 == number2:" , $number1 == $number2); // true
+var_dump("number1 <= number2:" , $number1 <= $number2); // true
+var_dump("number1 >= number2:" , $number1 >= $number2); // true
+var_dump("number1 < number2:" , $number1 < $number2); // false
+var_dump("number1 > number2:" , $number1 > $number2); // false
+var_dump("number1 === number2:" , $number1 === $number2); // true
+var_dump("text == number2:" , $text == $number2); // true
+var_dump("text === number2:" , $text === $number2); // false
+var_dump("number1 != number2:" , $number1 != $number2); // false
+var_dump("number1 !== number2:" , $number1 !== $number2); // false
+var_dump("text !== number2:" , $text !== $number2); // true
 
 
 
