b273d85c1a79aa073fd8616f3e615cdfeed5702f
bahmannikouei <nikoubahman@gmail.com>
2024-06-03 09:54:42 +0200

    Jalase 09

---
+++ jalase09/textAnalys.php
@@ -0,0 +1,53 @@
+<?php 
+$text = '';
+$chars = '';
+$words = '';
+$sent = '';
+$parag = '';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST'){
+    $text = $_POST['text'];
+    $chars = mb_strlen($text);
+    $words = str_word_count($text,0,'ابپتثجچحخدذرزژسشصضطظعغفقکگلمنوهیآ');
+    $sent =count(preg_split('/[?.!]\s/', $text,0),PREG_SPLIT_NO_EMPTY);
+    $parag =count(preg_split('/\n\s*/', $text,0),PREG_SPLIT_NO_EMPTY);
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <style>
+        body{
+            width: 800px;
+            background-color: wheat;
+            text-align: center;
+            direction: rtl;
+            margin: auto;
+        }
+        div{
+            text-align: right;
+            font-size: 20px;
+            padding-right: 100px;
+        }
+
+    </style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>آنالیز متن</title>
+</head>
+<body>
+    <h3>آنالیز متن</h3>
+    <form action="" method="post">
+    <textarea name="text" id="" rows="15" cols="100"><?php echo $text;?></textarea>
+    <input type="submit" name="submit" value="آنالیز متن">
+    </form>
+    <div>
+        تعداد کارکتر        <?php echo $chars ?> <br>
+        تعداد کلمه        <?php echo $words ?><br>
+        تعداد جمله        <?php echo $sent ?><br>
+        تعداد پاراگراف        <?php echo $parag ?><br>
+    </div>
+</body>
+</html>
